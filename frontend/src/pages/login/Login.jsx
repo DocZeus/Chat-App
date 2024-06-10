b93ec76b4df86110ce41ef5d7d42c784f29ff6d4
doc <swapniltoppo@gmail.com>
2024-06-10 13:11:37 +0530

    Frontend Login UI

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                    Login
+                    <span className='text-red-500'> ConvoTex</span>
+                </h1>
+                <form>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Username</span>
+                        </label>
+                        <input type='text' placeholder='Jack Sparrow' className='w-full input input-bordered h-10' />
+                    </div>
+                    <div>
+                        <label className='label'>
+                            <span className='text-base label-text'>Password</span>
+                        </label>
+                        <input type='password' placeholder='********' className='w-full input input-bordered h-10' />
+                    </div>
+                    <div className='text-sm'>
+                        Don&apos;t have an account?&nbsp;
+                        <a href='#' className='hover: underline text-blue-600 mt-2 inline-block'>
+                            Register Now
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
