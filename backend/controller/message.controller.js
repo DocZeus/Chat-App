60b01c705ed39ce4e436fc1698ed9ea43aa70d0c
doc <swapniltoppo@gmail.com>
2024-06-10 11:16:02 +0530

    Message Controller

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -42,3 +42,22 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages"); //Actual Message
+
+        if (!conversation) return res.status(200).json([]);
+
+        const messages = conversation.getMessages;
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
