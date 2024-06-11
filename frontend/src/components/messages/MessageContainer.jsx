15783ef2bbe162326ab23db376203c7a4b939602
doc <swapniltoppo@gmail.com>
2024-06-11 13:14:47 +0530

    Conversations

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,17 @@
+import Messages from "./Messages"
+
+const MessageContainer = () => {
+    return (
+        <div className="md:min-w-[450px] flex flex-col">
+            <>
+                <div className="bg-zinc-900 px-4 py-2 mb-2">
+                    <span className="label-text">To:</span> <span className="text-gray-300 font-bold">John</span>
+                </div>
+                <Messages />
+                {/* <MessageInput /> */}
+            </>
+        </div>
+    )
+}
+
+export default MessageContainer
