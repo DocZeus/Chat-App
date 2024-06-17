440eb9046ed4c626fd881076b6917a5ebb7d250e
doc <swapniltoppo@gmail.com>
2024-06-17 11:05:38 +0530

    Tweaks

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://convo-tex.onrender.com", {
+            const socket = io("https://convo-tex.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 },
